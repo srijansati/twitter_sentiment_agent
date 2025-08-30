41d1db4f4a6b6d85b7641c03c1259a7d4691b877
VASU <srijansati226@gmail.com>
2025-08-31 03:51:18 +0530

    crew addition and client definition

--- src/client.py
+++ src/client.py
@@ -1,1 +1,23 @@
-from crewai_tools
+from crewai_tools import MCPServerAdapter
+from crew import TwitterCrew
+
+server_params = {
+    "url" : "http://localhost:3000/sse",
+    "transport" : "sse"
+}
+
+def main():
+    with MCPServerAdapter(serverparams= server_params, connect_timeout= 60) as mcp_tools:
+        print(f"Available tools: {[tool.name for tool in mcp_tools]}")
+
+        
+        agent = TwitterCrew().data_scraper(tools= mcp_tools)
+
+        result = agent.kickoff(messages= 'Scrape data form twitter profile of any random user.')
+
+        print(result.raw)
+
+
+
+if __name__ == "__main__":
+    main()
--- src/crew.py
+++ src/crew.py
@@ -1,18 +1,23 @@
 from crewai import Agent, Crew, Process
 from crewai.project import CrewBase, crew, agent
-import yaml
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
 
-import crewai
 
 @CrewBase
 class TwitterCrew():
     """
     Agents for analyzing sentiment of twitter users.
     """
+    
+    agents : List[BaseAgent]
+
+
+
     agents_config = "config/agent.yaml"
 
     @agent
-    def data_scraper(self, tools):
+    def data_scraper(self, tools) -> Agent:
         return Agent(
             config= self.agents_config['data_scraper'],
             verbose= True,
@@ -20,17 +25,24 @@ class TwitterCrew():
         )
 
     @agent
-    def sentiment_analyzer(self):
+    def sentiment_analyzer(self) -> Agent:
         return Agent(
             config= self.agents_config['sentiment_analyzer'],
             verbose= True,
         )
 
     @agent
-    def report_generator(self):
+    def report_generator(self) -> Agent:
         return Agent(
             config= self.agents_config['report_generator'],
             verbose = True
         )
 
         
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents= self.agents,
+            process= Process.sequential,
+            verbose= True
+        )
